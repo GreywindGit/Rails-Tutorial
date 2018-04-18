98d937de9ceaf101d5a4a13b836db92b973e2625
Zsuzsa Rita Bogár <alassea.indilwen@gmail.com>
2018-04-18 11:17:05 +0200

    Add scaffolded Micropost model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Syntax: root 'controller_name#action_name' or get/post 'route' => 'controller_name#action_name'
 
-  root 'application#hello'
+  root 'users#index'
+  get '/hello' => 'application#hello'
   get '/goodbye' => 'application#goodbye'
 end
