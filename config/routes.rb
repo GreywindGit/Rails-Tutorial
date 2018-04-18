ddd2b994de2121b39628a8ab72dcecca8b6a80c0
Zsuzsa Rita Bogár <alassea.indilwen@gmail.com>
2018-04-18 10:32:19 +0200

    Add root and another route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Syntax: root 'controller_name#action_name' or get/post 'route' => 'controller_name#action_name'
+
+  root 'application#hello'
+  get '/goodbye' => 'application#goodbye'
 end
