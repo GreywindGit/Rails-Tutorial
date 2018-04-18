b9ceb04fc16e4d9484a400d45b9b46adbc429509
Zsuzsa Rita Bogár <alassea.indilwen@gmail.com>
2018-04-18 10:59:14 +0200

    Add scaffolded User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Syntax: root 'controller_name#action_name' or get/post 'route' => 'controller_name#action_name'
 
